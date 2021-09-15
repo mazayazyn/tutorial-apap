45c355a1e086f4408cbe8cba6f9a8105234defd0
indonesia mazaya <indonesiamazaya@gmail.com>
2021-09-15 17:04:35 +0700

    init

---
+++ 21cineplux/src/test/java/apap/tutorial/cineplux/service/BioskopInMemoryService.java
@@ -0,0 +1,31 @@
+package apap.tutorial.cineplux.service;
+
+import apap.tutorial.cineplux.model.BioskopModel;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BioskopInMemoryService implements BioskopService {
+    private List<BioskopModel> listBioskop;
+
+    //Constructor
+    public BioskopInMemoryService() {
+        listBioskop = new ArrayList<>();
+    }
+
+    @Override
+    public void addBioskop (BioskopModel bioskop) {
+        listBioskop.add(bioskop);
+    }
+
+    @Override
+    public List<BioskopModel> getBioskopList() {
+        return listBioskop;
+    }
+
+    @Override
+    public BioskopModel getBioskopByIdBioskop (String idBioskop) {
+        return listBioskop.get(listBioskop.indexOf(idBioskop));
+    }
+}
